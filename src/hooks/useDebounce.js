7708b9b9d42c7b4d0def4a234c7da74fb4518752
Harsha14 <harshavtadigadapa14@gmail.com>
2026-04-17 09:57:02 +0530

    useDebounce.js

--- src/hooks/useDebounce.js
+++ src/hooks/useDebounce.js
@@ -4,12 +4,12 @@ const useDebounce = (value, delay = 500) => {
   const [debouncedValue, setDebouncedValue] = useState(value);
 
   useEffect(() => {
-    // Set a timer to update debounced value after the delay
+    
     const timer = setTimeout(() => {
       setDebouncedValue(value);
     }, delay);
 
-    // Cleanup: cancel the timer if value changes before the delay is up
+    
     return () => clearTimeout(timer);
   }, [value, delay]);
 
